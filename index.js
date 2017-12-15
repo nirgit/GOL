f818faedc2006fd312a859354f555d7932c4575a
Nir Moav <nirm@wix.com>
2017-12-15 10:02:36 +0200

    initial commit for GOL

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 const start = () => {
     console.log('========== Game of Life simulation ==========');
+
+    const dimension = 10;
+    const board = createBoard(dimension);
+    const TOTAL_GENERATIONS = 5;
+    let generations = TOTAL_GENERATIONS;
+    
+    const play = () => {
+        if (generations > 0) {
+            drawBoard(TOTAL_GENERATIONS - generations, board);
+            updateBoard(board);
+            generations--;
+            setTimeout(play, 1000);
+        }
+    };
+    play();
 };
+
+const updateBoard = board => {
+    
+}
+
+const drawBoard = (generation, board) => {
+    console.log('\033[2J');
+    console.log('Generation ' + generation);
+    for (let i=0; i < board.length; i++) {
+        console.log(board[i].join(' ') + '\n');
+    }
+}
+
+const createBoard = (dimension) => {
+    const board = [];
+    for (var i=0; i < dimension; i++) {
+        board.push(Array(dimension));
+    }
+    return board;
+}
 
 start();
